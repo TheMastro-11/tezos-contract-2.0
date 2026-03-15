ef26b0e971dec1fb11b6559c2a7ff0ab8be1a812
AverageDeveloperOhio <65859573+TheMastro-11@users.noreply.github.com>
2026-03-15 20:26:07 +0100

    Refactor: Vesting Completed

--- contracts/Rosetta/Vesting/VestingRosetta.py
+++ contracts/Rosetta/Vesting/VestingRosetta.py
@@ -2,39 +2,65 @@ import smartpy as sp
 
 @sp.module
 def main():
+    import smartpy.stdlib.utils as utils
+    
     class VestingRosetta(sp.Contract):
-        def __init__(self, beneficiary, start_level, duration, total_amount):
-            self.data.beneficiary = sp.cast(beneficiary, sp.address)
-            self.data.start_level = sp.cast(start_level, sp.nat)
-            self.data.duration = sp.cast(duration, sp.nat)
-            self.data.total_amount = sp.cast(total_amount, sp.mutez)
-            self.data.released = sp.mutez(0)
+        def __init__(self, beneficiaryAddress: sp.address, start_timestamp: sp.nat, duration_seconds: sp.nat):
+            assert beneficiaryAddress != sp.address("0"), "Beneficiary is zero address"
+            self.data.released = sp.nat(0)
+            self.data.beneficiary = beneficiaryAddress
+            self.data.start = start_timestamp
+            self.data.duration = duration_seconds
 
         @sp.entrypoint
         def release(self):
-            assert sp.sender == self.data.beneficiary
-            assert sp.level >= self.data.start_level
-            assert self.data.duration > 0
-
-            elapsed = sp.as_nat(sp.level - self.data.start_level)
-            vested = sp.mutez(0)
-            if elapsed >= self.data.duration:
-                vested = self.data.total_amount
+            amount = sp.nat(0)
+            if (sp.level < self.data.start) :
+                amount = sp.nat(0)
             else:
-                vested = sp.split_tokens(self.data.total_amount, elapsed, self.data.duration)
+                if (sp.level > self.data.start + self.data.duration):
+                    amount =  utils.mutez_to_nat(sp.balance) + self.data.released
+                else:
+                    amount = ((utils.mutez_to_nat(sp.balance) + self.data.released) * sp.as_nat(sp.level - self.data.start)) / self.data.duration
+            amount = sp.as_nat(amount - self.data.released)
+            self.data.released = self.data.released + amount
+            sp.send(self.data.beneficiary, utils.nat_to_mutez(amount))
+            sp.emit(amount)
 
-            amount = vested - self.data.released
-            assert amount > sp.mutez(0)
-            self.data.released += amount
-            sp.send(self.data.beneficiary, amount)
+            
             
 @sp.add_test()
 def test():
-    #set scenario
     sc = sp.test_scenario("VestingRosetta", main)
-    #create users
-    beneficiary = sp.address("tz1SL2xBdmLSD2W3Hs84SfH912xDpYtAjsaa")
-    #create object
-    c1 = main.VestingRosetta(beneficiary, 5, 10, sp.mutez(10))
-    #start scenario
-    sc += c1
+
+    beneficiary = sp.test_account("beneficiary")
+    outsider = sp.test_account("outsider")
+    start_level = sp.nat(5)
+    duration = sp.nat(10)
+    initial_balance = sp.mutez(10_000_000)
+
+    vesting = main.VestingRosetta(beneficiary.address, start_level, duration)
+    vesting.set_initial_balance(initial_balance)
+    sc += vesting
+
+    sc.h1("VestingRosetta scenario")
+
+    sc.h2("Initial storage")
+    sc.verify(vesting.data.beneficiary == beneficiary.address)
+    sc.verify(vesting.data.start == start_level)
+    sc.verify(vesting.data.duration == duration)
+    sc.verify(vesting.data.released == sp.nat(0))
+    sc.verify(vesting.balance == initial_balance)
+
+    sc.h2("Static vesting configuration")
+    sc.verify(vesting.data.start + vesting.data.duration == sp.nat(15))
+    sc.verify(vesting.data.start < vesting.data.start + vesting.data.duration)
+    sc.verify(beneficiary.address != outsider.address)
+
+    sc.h2("Release entrypoint")
+    vesting.release(_sender=outsider.address, _level=1)
+    vesting.release(_sender=beneficiary.address, _level=5)
+    vesting.release(_sender=beneficiary.address, _level=20)
+
+    sc.verify(vesting.data.released == sp.nat(10000000))
+    sc.verify(vesting.balance == sp.mutez(0))
