7183d3abc909c0da9ca3650a11aa560521b6fef7
AverageDeveloperOhio <65859573+TheMastro-11@users.noreply.github.com>
2026-03-15 18:31:09 +0100

    Refactor: Storage Completed

--- contracts/Rosetta/Storage/StorageRosetta.py
+++ contracts/Rosetta/Storage/StorageRosetta.py
@@ -4,24 +4,50 @@ import smartpy as sp
 def main():
     class StorageRosetta(sp.Contract):
         def __init__(self):
-            self.data.stored_bytes = sp.bytes("0x")
-            self.data.stored_string = ""
+            self.data.byte_sequence = sp.cast(None, sp.option[sp.bytes])
+            self.data.text_string = sp.cast(None, sp.option[sp.string])
 
         @sp.entrypoint
-        def storeBytes(self, value):
-            sp.cast(value, sp.bytes)
-            self.data.stored_bytes = value
+        def storeBytes(self, byte_sequence: sp.bytes):
+            self.data.byte_sequence = sp.Some(byte_sequence)
 
         @sp.entrypoint
-        def storeString(self, value):
-            sp.cast(value, sp.string)
-            self.data.stored_string = value
+        def storeString(self, text_string: sp.string):
+            self.data.text_string = sp.Some(text_string)
 
 @sp.add_test()
 def test():
-    #set scenario
-    sc = sp.test_scenario("StorageRosetta",main)
-    #create object
-    Storage = main.StorageRosetta()
-    #start scenario
-    sc += Storage
+    sc = sp.test_scenario("StorageRosetta", main)
+
+    storage = main.StorageRosetta()
+    sc += storage
+
+    sc.h1("Main scenario")
+
+    sc.h2("Initial storage")
+    sc.verify(~storage.data.byte_sequence.is_some())
+    sc.verify(~storage.data.text_string.is_some())
+
+    sc.h2("Store bytes")
+    first_bytes = sp.bytes("0x1234")
+    second_bytes = sp.bytes("0xaabbccdd")
+
+    storage.storeBytes(first_bytes)
+    sc.verify(storage.data.byte_sequence.is_some())
+    sc.verify(storage.data.byte_sequence.unwrap_some() == first_bytes)
+    sc.verify(~storage.data.text_string.is_some())
+
+    storage.storeBytes(second_bytes)
+    sc.verify(storage.data.byte_sequence.unwrap_some() == second_bytes)
+
+    sc.h2("Store strings")
+    first_text = "Hello Rosetta"
+    second_text = "Storage updated"
+
+    storage.storeString(first_text)
+    sc.verify(storage.data.text_string.is_some())
+    sc.verify(storage.data.text_string.unwrap_some() == first_text)
+    sc.verify(storage.data.byte_sequence.unwrap_some() == second_bytes)
+
+    storage.storeString(second_text)
+    sc.verify(storage.data.text_string.unwrap_some() == second_text)
